3e30d9002a8cd24b1249d5861b934c0b9b3107ba
Elfall <elfall71@gmail.com>
2024-09-18 11:00:59 +0200

    prova3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          50: '#f5f3ff',
+          100: '#ede9fe',
+          // ... altri valori di purple
+        },
+        yellow: {
+          400: '#facc15',
+          // ... altri valori di yellow
+        },
+      },
+    },
   },
   plugins: [],
 }
